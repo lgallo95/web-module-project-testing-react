fadc41311e09f18989edfc2c47cc444de49c91d3
lgallo95 <lgallo2005@gmail.com>
2021-12-13 15:47:27 -0800

    finished episode and show

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,44 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const testData = {
+    id: 1,
+    name:'',
+    image: 'https://static.tvmaze.com/uploads/images/medium_landscape/342/855787.jpg',
+    season: "",
+    number: 1,
+    summary: "abc",
+    runtime: 1
+}
 
-test("renders without error", () => {});
+const testData2 = {
+    id: 1,
+    name:'',
+    image: null,
+    season: "",
+    number: 1,
+    summary: "abc",
+    runtime: 1
+}
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders without error", () => {
+    render(<Episode episode={testData} />);
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={testData} />);
+
+    const summary = screen.queryByText(/abc/i);
+
+    expect(summary).toBeInTheDocument()
+    expect(summary).toBeTruthy()
+    expect(summary).toHaveTextContent("abc")
+});
+
+test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={testData2}/>);
+
+    const image = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+
+    expect(image).toBeInTheDocument();
+});
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -4,14 +4,68 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
+import Episodes from '../Episodes';
 
-test('renders without errors', ()=>{});
+const testData ={
+    name: "Name",
+    summary: "Summary",
+    seasons: [
+        {
+            id: 0,
+            name: "S1",
+            episodes: []
+        },
+        {
+            id: 1,
+            name: "S2",
+            episodes: []
+        },
+    ]
+}
 
-test('renders Loading component when prop show is null', () => {});
+test('renders without errors', ()=>{
+    render(<Show show ={testData} selectedSeason = {'none'}/>)
+});
+
+test('renders Loading component when prop show is null', () => {
+    render(<Show show={null} />)
+
+    const loading = screen.queryByText(/Fetching data.../i)
+
+    expect(loading).toBeInTheDocument()
+});
 
 
-test('renders same number of options seasons are passed in', ()=>{});
+test('renders same number of options seasons are passed in', ()=>{
+    render(<Show show={testData} selectedSeason= {'none'} />)
 
-test('handleSelect is called when an season is selected', () => {});
+    const opt = screen.queryAllByTestId('season-option')
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {});
+    expect(opt).toHaveLength(2)
+});
+
+test('handleSelect is called when an season is selected', () => {
+    const handleSelect = jest.fn()
+    
+    render(<Show show={testData} selectedSeason= {'none'} handleSelect={handleSelect} />)
+
+    const slct = screen.getByLabelText(/Select a season/i)
+
+    userEvent.selectOptions(slct, ['1']);
+
+    expect(handleSelect).toBeCalled()
+});
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    const { rerender } =  render(<Show show={testData} selectedSeason= {'none'} />);
+    
+    let eps = screen.queryByTestId('episodes-container')
+    
+    expect(eps).not.toBeInTheDocument()
+
+    rerender(<Show show={testData} selectedSeason= {1} />)
+
+    eps = screen.queryByTestId('episodes-container')
+
+    expect(eps).toBeInTheDocument()
+});
